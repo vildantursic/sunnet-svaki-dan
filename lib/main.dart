121289a4b9e078c045b57a5386a882b1b71ecb1b
Vildan Tursic <vildantursic@hotmail.com>
2023-02-07 10:26:43 +0100

    ios build testings

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Sunnet Svaki Dan',
       theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
       darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
